a9d3425a8d3ec70711ecdb13a41030f35c948e67
Kaushik Gopal <code@kaush.co>
2013-02-25 22:28:39 -0800

    refactoring code after making sure it passes

--- kata4_coin_changer/coin_changer_spec.rb
+++ kata4_coin_changer/coin_changer_spec.rb
@@ -2,35 +2,27 @@ require "rspec"
 require "./vending_machine.rb"
 
 describe "Vending Machine" do
+  
   before(:all) do
     @vm = VendingMachine.new()
-    @denomination_array = [1, 5, 10, 25, 100]
   end
 
   it "should have a method that takes in three parameters" do
   	@vm.should respond_to(:get_num_of_coins).with(2).arguments
+  end  	
+  
+  {
+  	0 => [0, 0, 0, 0, 0],
+  	15 => [0, 1, 1, 0, 0],
+  	40 => [0, 1, 1, 1, 0],
+  	100 => [0, 0, 0, 0, 1],
+  	423 => [3, 0, 2, 0, 4]
+  }.each do |amount, output_array|
+		it "should return #{output_array} for an input of #{amount}" do
+			expect(		@vm.get_num_of_coins( amount, 
+																			[1, 5, 10, 25, 100])).to eql(	
+																			output_array )
+		end
   end
-  it "should return all 0s for 0" do
-  	expect(		@vm.get_num_of_coins( 0, 
-																		@denomination_array)).to eql(	
-																		[0, 0, 0, 0, 0] )
-  end
-
-  it "should return [0, 1, 1, 0, 0] for an input of 15" do
-		expect(		@vm.get_num_of_coins( 15, 
-																		@denomination_array)).to eql(	
-																		[0, 1, 1, 0, 0] )
-  end
-  it "should return [0, 1, 1, 1, 0] for an input of 40" do
-		expect(		@vm.get_num_of_coins( 40, 
-																		@denomination_array)).to eql(	
-																		[0, 1, 1, 1, 0] )
-  end
-  it "should return [3, 0, 2, 0, 4] for an input of 423" do
-		expect(		@vm.get_num_of_coins( 423, 
-																		@denomination_array)).to eql(	
-																		[3, 0, 2, 0, 4] )
-  end
-
 
 end
--- kata4_coin_changer/vending_machine.rb
+++ kata4_coin_changer/vending_machine.rb
@@ -2,17 +2,11 @@ class VendingMachine
 
 	def get_num_of_coins (total_amt, denomination_array)
 		
-		tmp_total_amt = total_amt
-		output = Array.new(denomination_array.length)
+		output = []		
 		
 		denomination_array.sort.reverse.each_with_index do |denomination, index|
-			if denomination > tmp_total_amt
-					output[index] = 0 
-			else
-					# we're storing it in the same place as the reversed denomination_array array, so remember to reverse in the end
-					output[index] = tmp_total_amt/denomination
-					tmp_total_amt = tmp_total_amt%denomination
-			end
+			output[index] = total_amt/denomination
+			total_amt = total_amt%denomination
 		end
 
 		output.reverse
