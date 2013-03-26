a9ae49afde4e2821ba7bc480038b8f1d470a72e7
Kaushik Gopal <code@kaush.co>
2013-03-26 01:06:08 -0700

    refactoring test cases

--- kata5_mars_rover/mars_rover_spec.rb
+++ kata5_mars_rover/mars_rover_spec.rb
@@ -11,156 +11,115 @@ describe MarsRover do
   end
 
   context "receives single commands" do
-    context "NORTH" do
-      before(:each) do
-        rover.direction = "N"
-      end
-      it "should move one position in the positive y coordinate for 'f'" do
-        rover.coordinates = [3,3]
-        rover.move(["f"])
-        expect(rover.coordinates).to eql([3,4])
-      end  
-      it "should move one position in the negative y axis for 'b'" do
-        rover.coordinates = [3,3]
-        rover.move(["b"])
-        expect(rover.coordinates).to eql([3,2])
-      end
-      it "should change the position to East when given 'r'" do
-        rover.move(["r"])
-        expect(rover.direction).to eql("E")
-      end
-      it "should change the position to West when given 'l'" do
-        rover.move(["l"])
-        expect(rover.direction).to eql("W")
+    it "should move correctly when pointing to the North direction" do
+      [
+        [[3,3], ["f"], [3,4], "N"],
+        [[3,3], ["b"], [3,2], "N"],
+        [[3,3], ["r"], [3,3], "E"],
+        [[3,3], ["l"], [3,3], "W"]
+      ].each do |coordinates, command, expected_position, expected_direction|
+        rover.reset_rover_position
+        rover.coordinates = coordinates
+        
+        rover.move(command)
+        expect(rover.coordinates).to eql(expected_position)
+        expect(rover.direction).to eql(expected_direction)
       end
     end
 
-    context "SOUTH" do
-      before(:each) do
+    it "should move correctly when pointing to the South direction" do
+      [
+        [[3,3], ["f"], [3,2], "S"],
+        [[3,3], ["b"], [3,4], "S"],
+        [[3,3], ["r"], [3,3], "W"],
+        [[3,3], ["l"], [3,3], "E"]
+      ].each do |coordinates, command, expected_position, expected_direction|
         rover.direction = "S"
-      end
-      it "should move one position in the negative y coordinate for 'f'" do
-        rover.coordinates = [3,3]
-        rover.move(["f"])
-        expect(rover.coordinates).to eql([3,2])
-      end  
-      it "should move one position in the positive y axis for 'b'" do
-        rover.coordinates = [3,3]
-        rover.move(["b"])
-        expect(rover.coordinates).to eql([3,4])
-      end
-      it "should change the position to West when given 'r'" do
-        rover.move(["r"])
-        expect(rover.direction).to eql("W")
-      end
-      it "should change the position to East when given 'l'" do
-        rover.move(["l"])
-        expect(rover.direction).to eql("E")
-      end
+        rover.coordinates = coordinates
+        
+        rover.move(command)
+        expect(rover.coordinates).to eql(expected_position)
+        expect(rover.direction).to eql(expected_direction)
+      end 
     end
 
-    context "EAST" do
-      before(:each) do
+    it "should move correctly when pointing to the East direction" do
+       [
+        [[3,3], ["f"], [4,3], "E"],
+        [[2,2], ["b"], [1,2], "E"],
+        [[3,3], ["r"], [3,3], "S"],
+        [[3,3], ["l"], [3,3], "N"]
+      ].each do |coordinates, command, expected_position, expected_direction|
         rover.direction = "E"
-      end
-      it "should move one position in the positive x coordinate for 'f'" do
-        rover.coordinates = [3,3]
-        rover.move(["f"])
-        expect(rover.coordinates).to eql([4,3])
-      end
-      it "should move one position in the negative x coordinate for 'b'" do
-        rover.coordinates = [2,2]
-        rover.move(["b"])
-        expect(rover.coordinates).to eql([1,2])
-      end  
-      it "should change the position to South when given 'r'" do
-        rover.move(["r"])
-        expect(rover.direction).to eql("S")
-      end
-      it "should change the position to North when given 'l'" do
-        rover.move(["l"])
-        expect(rover.direction).to eql("N")
-      end
+        rover.coordinates = coordinates
+        
+        rover.move(command)
+        expect(rover.coordinates).to eql(expected_position)
+        expect(rover.direction).to eql(expected_direction)
+      end 
     end
 
-    context "WEST" do
-      before(:each) do
+    it "should move correctly when pointing to the West direction" do
+       [
+        [[3,3], ["f"], [2,3], "W"],
+        [[2,2], ["b"], [3,2], "W"],
+        [[3,3], ["r"], [3,3], "N"],
+        [[3,3], ["l"], [3,3], "S"]
+      ].each do |coordinates, command, expected_position, expected_direction|
         rover.direction = "W"
-      end
-      it "should move one position in the negative x coordinate for 'f'" do
-        rover.coordinates = [3,3]
-        rover.move(["f"])
-        expect(rover.coordinates).to eql([2,3])
-      end
-      it "should move one position in the positive x coordinate for 'b'" do
-        rover.coordinates = [2,2]
-        rover.move(["b"])
-        expect(rover.coordinates).to eql([3,2])
-      end  
-      it "should change the position to North when given 'r'" do
-        rover.move(["r"])
-        expect(rover.direction).to eql("N")
-      end
-      it "should change the position to South when given 'l'" do
-        rover.move(["l"])
-        expect(rover.direction).to eql("S")
-      end
+        rover.coordinates = coordinates
+        
+        rover.move(command)
+        expect(rover.coordinates).to eql(expected_position)
+        expect(rover.direction).to eql(expected_direction)
+      end 
     end
   end
 
   it "should recieve a character array of commands" do
-    rover.move(["ffrff"])
-    expect(rover.coordinates).to eql([2,2])
+    [
+        [[0,0], "N", ["ffrff"], [2,2], "E"],
+        [[1,1], "N", ["ffrff"], [3,3], "E"],
+        [[3,4], "S", ["ffbrf"], [2,3], "W"]
+
+    ].each do |coordinates, direction , command_array, expected_position, expected_direction|
+      rover.direction = direction
+      rover.coordinates = coordinates
+      
+      rover.move(command_array)
+
+      expect(rover.coordinates).to eql(expected_position)
+      expect(rover.direction).to eql(expected_direction)
+    end 
   end
 
-  context "GRID WRAPPING" do
-    before(:each) do
-      rover.boundary = [100,100]
-    end
-    it "should wrap to the top of the grid if it goes below 0 for a South facing forward direction" do
-      rover.direction = "S"
-      rover.move(["f"])
-      expect(rover.coordinates).to eql([0,100])
-    end
-    it "should wrap to the top of the grid if it goes below 0 for a North facing backward direction" do
-      rover.direction = "N"
-      rover.move(["b"])
-      expect(rover.coordinates).to eql([0,100])
-    end
-    it "should wrap to the bottom of the grid it goes above boundary for a North facing forward direction" do
-      rover.coordinates = [4,100]
-      rover.direction = "N"
-      rover.move(["f"])
-      expect(rover.coordinates).to eql([4,0])
-    end
-    it "should wrap to the bottom of the grid it goes above boundary for a South facing backward direction" do
-      rover.coordinates = [4,100]
-      rover.direction = "S"
-      rover.move(["b"])
-      expect(rover.coordinates).to eql([4,0])
-    end
+  it "should wrap around the grid correctly" do
+    rover.boundary = [100,100]
+    [
+      [[0,0], "S", ["f"], [0,100]],
+      [[0,0], "N", ["b"], [0,100]],
+      [[4,100], "N", ["f"], [4,0]],
+      [[4,100], "S", ["b"], [4,0]],
+      
+      [[0,0], "W", ["f"], [100,0]],
+      [[0,0], "E", ["b"], [100,0]],
+      [[100,4], "E", ["f"], [0,4]],
+      [[100,4], "W", ["b"], [0,4]]
 
-    it "should wrap to the right of the grid if it goes below 0 for a West facing forward direction" do
-      rover.direction = "W"
-      rover.move(["f"])
-      expect(rover.coordinates).to eql([100,0])
+    ].each do |initial_coordinates, direction, command, expected_position|
+        rover.coordinates = initial_coordinates
+        rover.direction = direction
+        rover.move(command)
+        expect(rover.coordinates).to eql(expected_position)
     end
-    it "should wrap to the right of the grid if it goes below 0 for a East facing backward direction" do
-      rover.direction = "E"
-      rover.move(["b"])
-      expect(rover.coordinates).to eql([100,0])
-    end
-    it "should wrap to the left of the grid if it goes above boundary for a East facing forward direction" do
-      rover.coordinates = [100,4]
-      rover.direction = "E"
-      rover.move(["f"])
-      expect(rover.coordinates).to eql([0,4])
-    end
-    it "should wrap to the left of the grid if it goes above boundary for a West facing backward direction" do
-      rover.coordinates = [100,4]
-      rover.direction = "W"
-      rover.move(["b"])
-      expect(rover.coordinates).to eql([0,4])
+  end
+
+
+  context "Obstacle detection" do
+    let(:obstacle){Obstacle.new([1,1], [1,2])}
+    xit "should stop at an obstacle" do
+      rover.move(["frff"])
+      expect(rover.coordinates).to eql([0,1])
     end
   end
 
