854c418d4f0986342b4757df64be670c09dbff02
Kaushik Gopal <code@kaush.co>
2013-06-03 14:29:17 -0700

    Refactoring

--- kata7_pig_latin/pig_latin.rb
+++ kata7_pig_latin/pig_latin.rb
@@ -18,35 +18,35 @@ class PigLatin
 	end
 
 	def starts_with_normal_chars?(word)
-		(word[0] =~ /^[[:alpha:]]/)
-	end
-
-	def perform_consonant_replacement(word)
-		word_array = word.split("")
-		shift_char_count = 0
-		was_prev_char_q = (word[0].downcase == "q")
-
-		word_array.each do |char|
-			shift_char_count += 1 if !starts_with_vowel?(char) || was_prev_char_q
-			break if starts_with_vowel?(char) && !was_prev_char_q
-			was_prev_char_q = (char.downcase == "q")
-		end
-
-		word_array.push(	word_array.shift(shift_char_count)
-																.map(&:downcase)
-																.push("ay")
-																.join)
-
-		word_array[0] = word_array[0].upcase if starts_with_upcase?(word)
-		word_array.join
-	end
-
-	def perform_vowel_replacement(word)
-		word << "way"
+		word[0] =~ /^[[:alpha:]]/
 	end
 
 	def starts_with_upcase?(word)
 		word[0] == word[0].upcase
 	end
 
+	def perform_vowel_replacement(word)
+		word << "way"
+	end
+
+	def perform_consonant_replacement(word)
+		word_array = word.split("")
+		word_array.push(word_array.shift(get_number_of_consonants_to_shift(word))
+															.map(&:downcase))
+		word_array[0] = word_array[0].upcase if starts_with_upcase?(word)
+		word_array.push("ay").join
+	end
+
+	def get_number_of_consonants_to_shift(word)
+		num_of_chars_to_shift = 0
+		prev_char_was_q = (word[0].downcase == "q")
+
+		word.split("").each do |char|
+			num_of_chars_to_shift += 1 if !starts_with_vowel?(char) || prev_char_was_q
+			break if starts_with_vowel?(char) && !prev_char_was_q
+			prev_char_was_q = (char.downcase == "q")
+		end
+
+		num_of_chars_to_shift
+	end
 end
