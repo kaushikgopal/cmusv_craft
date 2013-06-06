839ebb139e0037f94dc166300aca9a427f9bf2c4
Kaushik Gopal <code@kaush.co>
2013-06-05 17:27:37 -0700

    Adding an implementation for mutator version of map!

--- kg_ruby_scripts/meta_programming/implement_map_using_each.rb
+++ kg_ruby_scripts/meta_programming/implement_map_using_each.rb
@@ -31,3 +31,19 @@ end
 mapped = [1, 2, 3].my_map{|i| i + 1}
 puts mapped.inspect
 # => [2, 3, 4]
+
+# and a mutator version
+
+class Array
+  def map!(proc_object)
+    self.each_with_index do |value, index|
+      self[index] = proc_object.call(value)
+    end
+  end
+end
+
+number_squared = Proc.new { |n| n * n }
+array = [1,2,3,4]
+
+array.map!(number_squared)
+# => [1, 4, 9, 16]
